44647b4bfc95c9e9f89d419eb862d277a2b7b2ec
dimon@mindset-test.com <123456789>
2019-03-16 19:12:00 +0300

    FindBook API - add books tests

--- tests/Acceptance/BookTest.php
+++ tests/Acceptance/BookTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Acceptance;
 
+use App\Models\Category;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 /** @group Acceptance */
@@ -45,5 +47,62 @@ class BookTest extends TestCase
         $searchString = Str::random(rand(3, 8));
         $request = $this->get('api/search?search=' . $searchString, $this->header);
         $request->assertStatus(200);
+    }
+
+    public function testBooks_ByLatitudeAndLongitude_success()
+    {
+        $latitude = $longitude = 10.10;
+        $response = $this->get('api/books?latitude=' . $latitude . '&longitude=' . $longitude);
+        $response->assertStatus(200);
+
+    }
+
+    public function testBooks_ByLatitudeAndLongitude_failed_noLatitude()
+    {
+        $longitude = 10.10;
+        $response = $this->get('api/books?longitude=' . $longitude, $this->header);
+        $response->assertStatus(422);
+    }
+
+    public function testBooks_ByLatitudeAndLongitude_failed_noLongitude()
+    {
+        $latitude = 10.10;
+        $response = $this->get('api/books?latitude=' . $latitude, $this->header);
+        $response->assertStatus(422);
+    }
+
+    public function testBooksBySquare_success()
+    {
+        $top = $bottom = $left = $right = 10.10;
+        $response = $this->get('api/books?square_top=' . $top . '&square_bottom=' . $bottom . '&square_left=' . $left . '&square_right=' . $right, $this->header);
+        $response->assertStatus(200);
+    }
+
+    public function testBooksBySquare_failed_noBottom()
+    {
+        $top = $left = $right = 10.10;
+        $response = $this->get('api/books?square_top=' . $top . '&square_left=' . $left . '&square_right=' . $right, $this->header);
+        $response->assertStatus(422);
+    }
+
+    public function testBooksBySquare_failed_noTop()
+    {
+        $bottom = $left = $right = 10.10;
+        $response = $this->get('api/books?&square_bottom=' . $bottom . '&square_left=' . $left . '&square_right=' . $right, $this->header);
+        $response->assertStatus(422);
+    }
+
+    public function testBooksBySquare_failed_noLeft()
+    {
+        $top = $bottom = $right = 10.10;
+        $response = $this->get('api/books?square_top=' . $top . '&square_bottom=' . $bottom . '&square_right=' . $right, $this->header);
+        $response->assertStatus(422);
+    }
+
+    public function testBooksBySquare_failed_noRight()
+    {
+        $top = $bottom = $left = 10.10;
+        $response = $this->get('api/books?square_top=' . $top . '&square_bottom=' . $bottom . '&square_left=' . $left, $this->header);
+        $response->assertStatus(422);
     }
 }
