79a1376629d7bdd9d1813d39118b45dbe39a42d7
Dmitry Meshkov <kobe8.0@mail.ru>
2019-10-11 22:14:00 +0300

    FindBook API - code style changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
-//            CreatorSeeder::class,
+            CreatorSeeder::class,
             UserSeeder::class,
         ]);
     }
